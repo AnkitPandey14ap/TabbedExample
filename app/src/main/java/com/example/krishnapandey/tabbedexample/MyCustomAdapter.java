6c5756f66fe57bf62a362195d3aa56eca02ecbc8
Ankitpandey14ap <ankit.pandey14ap@gmail.com>
2017-07-22 22:20:40 +0530

    Initial commit

---
+++ app/src/main/java/com/example/krishnapandey/tabbedexample/MyCustomAdapter.java
@@ -0,0 +1,62 @@
+package com.example.krishnapandey.tabbedexample;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by krishna pandey on 22-07-2017.
+ */
+
+class MyCustomAdapter extends BaseAdapter implements ListAdapter{
+
+    private final Context context;
+    private final ArrayList<String> list;
+
+    public MyCustomAdapter(Context context, ArrayList<String> list) {
+
+        this.context = context;
+        this.list = list;
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return list.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view=convertView;
+        if(view==null){
+            LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = layoutInflater.inflate(R.layout.item_topic_view, null);
+        }
+        TextView textView = (TextView) view.findViewById(R.id.textView);
+        textView.setText(list.get(position));
+
+
+        return view;
+    }
+}
